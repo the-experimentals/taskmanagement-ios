fb99fdae73c5ca76f71d6f97cbfe8cd49c1f92b5
Bibek Saini <itsbibeksaini@gmail.com>
2023-09-26 11:12:23 -0400

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Bibek Saini on 2023-09-26.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
